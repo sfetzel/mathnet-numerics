82a872d655ad8cc82bd51d79ba03b8b74281e3c6
Simon Fetzel <simonf93@univie.ac.at>
2022-12-28 00:53:18 +0100

    implemented GivensRotation, factorization

--- src/Numerics.Tests/LinearAlgebraTests/Complex/Solvers/AmvwSolverTest.cs
+++ src/Numerics.Tests/LinearAlgebraTests/Complex/Solvers/AmvwSolverTest.cs
@@ -32,10 +32,12 @@ using System.Collections.Generic;
 using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.LinearAlgebra.Complex;
 using MathNet.Numerics.LinearAlgebra.Complex.Solvers;
+using MathNet.Numerics.Providers.LinearAlgebra;
 using NUnit.Framework;
 
 namespace MathNet.Numerics.Tests.LinearAlgebraTests.Complex
 {
+    using static MathNet.Numerics.LinearAlgebra.Complex.Solvers.AmvwSolver;
     using Complex = System.Numerics.Complex;
 
     /// <summary>
@@ -44,15 +46,129 @@ namespace MathNet.Numerics.Tests.LinearAlgebraTests.Complex
     [TestFixture, Category("LA")]
     public class AmvwSolverTest
     {
-        [Test]
-        public void CalculateGivensRotationTest()
+        /// <summary>
+        /// Checks if the method GivensRotation.Create calculates a rotation,
+        /// which zeros the second component of the vector (1+2j, 3+4j).
+        /// </summary>
+        [TestCase(1, 2, 3, 4)]
+        [TestCase(1, 2, 0, 0)]
+        public void CalculateGivensRotationTest(double ar, double ai, double br, double bi)
         {
-            (Complex a, Complex b) = (new Complex(1, 2), new Complex(3, 4));
-            (AmvwSolver.GivensRotation rotation, double norm) = AmvwSolver.GivensRotation.Create(a, b);
-            Assert.AreEqual(Math.Sqrt(5 + 25), norm);
-            (Complex c, Complex d) = rotation * (a, b);
-            Assert.AreEqual(norm, c);
-            Assert.AreEqual(0, d);
+            (var a, var b) = (new Complex(ar, ai), new Complex(br, bi));
+            (var rotation, var norm) = AmvwSolver.GivensRotation.Create(ref a, ref b);
+            Assert.That(norm, Is.EqualTo(Math.Sqrt(ar*ar + ai*ai + br*br + bi*bi)));
+            (var c, var d) = rotation * (new Complex(ar, ai), new Complex(br, bi));
+            Assert.Multiple(() =>
+            {
+                Assert.That(c.Magnitude, Is.EqualTo(norm).Within(1e-12));
+                Assert.That(a.Magnitude, Is.EqualTo(norm).Within(1e-12));
+                Assert.That(d.Magnitude, Is.EqualTo(0).Within(1e-12));
+                Assert.That(b.Magnitude, Is.EqualTo(0).Within(1e-12));
+            });
+        }
+
+        [Test]
+        public void CreateUnitaryMatrix()
+        {
+            Complex[] vector = new Complex[]
+            {
+                new Complex(1, 2),
+                new Complex(3, 4),
+                new Complex(5, 6),
+                new Complex(7, 8)
+            };
+            var copy = new Complex[vector.Length];
+            Array.Copy(vector, copy, vector.Length);
+
+            var norm = Math.Sqrt(vector.Select(x => x.MagnitudeSquared()).Sum());
+            var matrix = AmvwSolver.UnitaryMatrix.Create(vector);
+            
+            // apply transformations to original vector
+            for(var i = matrix.CoreTransformations.Length-1; i >= 0; --i)
+            {
+                var rotation = matrix.CoreTransformations[i];
+                (copy[i], copy[i + 1]) = rotation * (copy[i], copy[i + 1]);
+            }
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(copy[0].Magnitude, Is.EqualTo(norm).Within(1e-12));
+                Assert.That(copy[1].Magnitude, Is.EqualTo(0).Within(1e-12));
+                Assert.That(copy[2].Magnitude, Is.EqualTo(0).Within(1e-12));
+                Assert.That(copy[3].Magnitude, Is.EqualTo(0).Within(1e-12));
+            });
+        }
+
+        [Test]
+        public void CreateFactorization()
+        {
+            var coefficients = new Complex[]
+            {
+                new Complex(1, 2),
+                new Complex(3, 4),
+                new Complex(5, 6)
+            };
+            var factorization = AmvwFactorization.Create(coefficients);
+
+            // The factorization should be equal to the companion matrix.
+            (var a, var b, var c, var d) = factorization.GetPart(0);
+            Assert.Multiple(() =>
+            {
+                Assert.That(a.Magnitude, Is.EqualTo(0).Within(1e-12));
+                Assert.That(c.Real, Is.EqualTo(1).Within(1e-12));
+                Assert.That(c.Imaginary, Is.EqualTo(0).Within(1e-12));
+                Assert.That(b.Magnitude, Is.EqualTo(0).Within(1e-12));
+                Assert.That(d.Magnitude, Is.EqualTo(0).Within(1e-12));
+            });
+        }
+
+        [Test]
+        public void CreateVectorX()
+        {
+            var coefficients = new Complex[]
+            {
+                new Complex(1, 2),
+                new Complex(3, 4),
+                new Complex(5, 6),
+                new Complex(7, 8)
+            };
+            var x = AmvwFactorization.CreateVectorX(coefficients);
+            Assert.Multiple(() =>
+            {
+                Assert.That(x[0], Is.EqualTo(-coefficients[1]));
+                Assert.That(x[1], Is.EqualTo(-coefficients[2]));
+                Assert.That(x[2], Is.EqualTo(-coefficients[3]));
+                Assert.That(x[3], Is.EqualTo(-coefficients[0]));
+                Assert.That(x[4], Is.EqualTo(Complex.One));
+            });
+        }
+
+        [Test]
+        public void Turnover()
+        {
+            (GivensRotation rotation1, _) = GivensRotation.Create(new Complex(1, 2), new Complex(3, 4));
+            (GivensRotation rotation2, _) = GivensRotation.Create(new Complex(4, 5), new Complex(6, 7));
+            (GivensRotation rotation3, _) = GivensRotation.Create(new Complex(7, 8), new Complex(9, 10));
+            var a = new Complex(3, 5);
+            var b = new Complex(7, 2);
+            var c = new Complex(9, 12);
+
+            Complex d = a;
+            Complex e = b;
+            Complex f = c;
+            
+            (d, e) = rotation3 * (d, e);
+            (e, f) = rotation2 * (e, f);
+            (d, e) = rotation1 * (d, e);
+
+            (var rotation4, var rotation5, var rotation6) = GivensRotation.Turnover(rotation1, rotation2, rotation3);
+
+            (b, c) = rotation6 * (b, c);
+            (a, b) = rotation5 * (a, b);
+            (b, c) = rotation4 * (b, c);
+
+            Assert.AreEqual(d.Real, a.Real, 1e-12);
+            Assert.AreEqual(d.Imaginary, a.Imaginary, 1e-12);
         }
     }
 }
--- src/Numerics/LinearAlgebra/Complex/Solvers/AmvwSolver.cs
+++ src/Numerics/LinearAlgebra/Complex/Solvers/AmvwSolver.cs
@@ -1,4 +1,4 @@
-// <copyright file="BiCgStab.cs" company="Math.NET">
+﻿// <copyright file="AmvwSolver.cs" company="Math.NET">
 // Math.NET Numerics, part of the Math.NET Project
 // http://numerics.mathdotnet.com
 // http://github.com/mathnet/mathnet-numerics
@@ -32,12 +32,20 @@ using MathNet.Numerics.LinearAlgebra.Solvers;
 using System.Collections.Generic;
 using System.Linq;
 using Complex = System.Numerics.Complex;
+using System.Reflection.Metadata.Ecma335;
 
 namespace MathNet.Numerics.LinearAlgebra.Complex.Solvers
 {
     using Complex = System.Numerics.Complex;
+    /// <summary>
+    /// Calculates the roots of a complex polynomial p(z) = z^n + a_{n-1}z^{n-1} + ... + a_1 z + a_0
+    /// using the algorithm described by [1]. The coefficient vector is therefore of length n.
+    /// </summary>
     public class AmvwSolver
     {
+        /// <summary>
+        /// Represents a Givens Rotator, which corresponds to the matrix [c, s; -s, c'].
+        /// </summary>
         public class GivensRotation
         {
             public Complex Cosine { get; set; }
@@ -45,7 +53,7 @@ namespace MathNet.Numerics.LinearAlgebra.Complex.Solvers
 
             public static (Complex a, Complex b) operator *(GivensRotation rotation, (Complex a, Complex b) tuple)
             {
-                return (tuple.a * rotation.Cosine + tuple.b * rotation.Sine, tuple.b * rotation.Cosine - tuple.a * rotation.Sine);
+                return (tuple.a * rotation.Cosine + tuple.b * rotation.Sine, tuple.b * rotation.Cosine.Conjugate() - tuple.a * rotation.Sine);
             }
 
             public GivensRotation(Complex cosine, double sine)
@@ -56,31 +64,192 @@ namespace MathNet.Numerics.LinearAlgebra.Complex.Solvers
 
             public static (GivensRotation Rotation, double norm) Create(Complex a, Complex b)
             {
-                var bNorm = b.Magnitude();
+                var bNorm = b.Magnitude;
+                var cosine = Complex.One;
+                var sine = 0.0;
                 var alpha = Math.Sqrt(a.MagnitudeSquared() + bNorm * bNorm);
-                // the rotation must be described with a complex cosine
-                // and a real sine. Basically this is done by factoring out the sine part.
 
-                // calculating cosine and sine.
-                var scaledA = a / alpha;
-                var scaledB = b / alpha;
+                if (bNorm != 0 && alpha != 0)
+                {
+                    // the rotation must be described with a complex cosine
+                    // and a real sine.
+                    // This is done by factoring out the phase of the sine part.
 
-                // rotation would be given by (scaledA, scaledB). Factor out scaledB'.
-                var cosine = scaledA * (b / bNorm).Conjugate() / alpha;
-                var sine = bNorm / alpha;
+                    cosine = (a * (b / bNorm).Conjugate() / alpha).Conjugate();
+                    sine = bNorm / alpha;
+                }
                 return (new GivensRotation(cosine, sine), alpha);
             }
-            
+
+            public static (GivensRotation Rotation, double norm) Create(ref Complex a, ref Complex b)
+            {
+                (var rotation, var alpha) = Create(a, b);
+                a = rotation.Cosine * a + b * rotation.Sine;
+                b = 0;
+                
+                return (rotation, alpha);
+            }
+
             public GivensRotation()
             {
                 Cosine = 1;
                 Sine = 0;
             }
+
+            public static (GivensRotation first, GivensRotation second, GivensRotation third) Turnover(GivensRotation leftRotation, GivensRotation middleRotation, GivensRotation rightRotation)
+            {
+                // calculate a turnover on the second and third row of first column
+
+                // t21 is the content of first column, second row.
+                var t21 = leftRotation.Sine * rightRotation.Cosine + leftRotation.Cosine.Conjugate() * middleRotation.Cosine * rightRotation.Sine;
+                var t31 = new Complex(middleRotation.Sine * rightRotation.Sine, 0);
+
+                (var firstRotation, var norm1) = GivensRotation.Create(ref t21, ref t31);
+
+                var t11 = leftRotation.Cosine * rightRotation.Cosine - leftRotation.Sine * middleRotation.Cosine * rightRotation.Sine;
+                // t21 contains now norm1 because of the call to GivensRotation.Create.
+
+                (var secondRotation, _) = GivensRotation.Create(ref t11, ref t21);
+
+                var t23 = middleRotation.Cosine * firstRotation.Cosine + leftRotation.Cosine * middleRotation.Sine * firstRotation.Sine;
+                // ensuring zero imaginary part by explicitly calculating the real part.
+                var t33 = new Complex(leftRotation.Sine * middleRotation.Sine * secondRotation.Sine
+                    - firstRotation.Sine * (secondRotation.Cosine.Real * middleRotation.Cosine.Real + secondRotation.Cosine.Imaginary * middleRotation.Cosine.Imaginary)
+                    + middleRotation.Sine * (
+                        leftRotation.Cosine.Real * (firstRotation.Cosine.Real * secondRotation.Cosine.Real + firstRotation.Cosine.Imaginary * secondRotation.Cosine.Imaginary)
+                        + firstRotation.Cosine.Imaginary * (firstRotation.Cosine.Real * secondRotation.Cosine.Imaginary - firstRotation.Cosine.Imaginary * secondRotation.Cosine.Real)
+                    ), 0);
+
+                (var thirdRotation, _) = GivensRotation.Create(ref t23, ref t33);
+
+                return (firstRotation, secondRotation, thirdRotation);
+            }
         }
 
         public class UnitaryMatrix
         {
-            public Dictionary<int, GivensRotation> CoreTransformations = new Dictionary<int, GivensRotation>();
+            public GivensRotation[] CoreTransformations { get; set; }
+
+            public UnitaryMatrix(GivensRotation[] rotations)
+            {
+                CoreTransformations = rotations;
+            }
+
+            /// <summary>
+            /// Creates a unitary matrix Q of givens rotations,
+            /// which transforms the vector v to a multiple of its norm multiplied with e_1.
+            /// </summary>
+            /// <param name="vector"></param>
+            /// <returns></returns>
+            public static UnitaryMatrix Create(Complex[] vector)
+            {
+                var n = vector.Length;
+                GivensRotation[] rotations = new GivensRotation[n-1];
+                for(int i = n-1; i > 0; --i)
+                {
+                    (rotations[i - 1], _) = GivensRotation.Create(ref vector[i - 1], ref vector[i]);
+                }
+                return new UnitaryMatrix(rotations);
+            }
+        }
+
+        /// <summary>
+        /// Represents the factorization A = Q * C^H * (B + e1*y^T)
+        /// </summary>
+        public class AmvwFactorization
+        {
+            /// <summary>
+            /// Represents the Q part of the factorization. The i-th transformation
+            /// is active on rows i and i+1.
+            /// </summary>
+            public UnitaryMatrix Q { get; set; }
+
+            public Complex[] D { get; set; }
+
+            /// <summary>
+            /// Represents the hermitian C matrix of the factorization. The i-th transformation
+            /// is active on rows (n-(i+1), n-i).
+            /// </summary>
+            public UnitaryMatrix C { get; set; }
+
+            /// <summary>
+            /// Represents the B part of the factorization. The i-th transformation
+            /// is active on rows (i, i+1).
+            /// </summary>
+            public UnitaryMatrix B { get; set; }
+
+            public int n { get; set; }
+
+            private AmvwFactorization() { }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="coefficients">The coefficient vector starting with a_0.</param>
+            /// <returns></returns>
+            public static AmvwFactorization Create(Complex[] coefficients)
+            {
+                var m = coefficients.Length;
+                var Q = new GivensRotation[m];
+                var D = new Complex[m];
+
+                for (var i = 0; i < m - 1; ++i)
+                {
+                    Q[i] = new GivensRotation(0, -1);
+
+                    D[i] = Complex.One;
+                }
+                Q[m - 1] = new GivensRotation(1, 0);
+                Complex[] x = CreateVectorX(coefficients);
+
+                var B = UnitaryMatrix.Create(x);
+                var C = new UnitaryMatrix(B.CoreTransformations.
+                    Select(x => new GivensRotation(x.Cosine.Conjugate(), x.Sine)).Reverse().ToArray());
+
+                return new AmvwFactorization() { B = B, C = C, D = D, Q = new(Q), n = m };
+            }
+
+            public static Complex[] CreateVectorX(Complex[] coefficients)
+            {
+                var m = coefficients.Length;
+                // create vector x = (-a_1, -a_2, ..., -a_{n-1}, -a_0, 1)
+                var x = new Complex[m + 1];
+                for (var i = 0; i < m - 1; ++i)
+                {
+                    x[i] = -coefficients[i + 1];
+                }
+                x[m - 1] = -coefficients[0];
+                x[m] = 1;
+                return x;
+            }
+
+            public (Complex a, Complex b, Complex c, Complex d) GetPart(int i)
+            {
+                // Calculate R(i:i+1, i:i+1) = [a, b; c, d]
+
+                // Using equation 4.14 from the paper.
+                // H = B + e_1 * y therefore H corresponds to B.
+                // h_{{i+1},i} = c_{{i+1},i} * r_{i,i}
+
+                // r_{i,i}
+                Complex a = B.CoreTransformations[i].Sine / C.CoreTransformations[n - 1 - i].Sine;
+                // r_{i+1,i+1}
+                Complex d = B.CoreTransformations[i + 1].Sine / C.CoreTransformations[n - 1 - (i + 1)].Sine;
+
+                // h_{i,i} = c_{i,i-1} * r_{i-1,i} + c_{i,i} * r_{i,i}
+                // r_{i-1,i} = (h_{i,i} - c_{i,i} * r_{i,i}) / c_{i,i-1}
+                // r_{i,i+1} = (h_{i+1, i+1} - c_{i+1, i+1} * r_{i+1, i+1}) / c_{i+1, i}
+                Complex b = (B.CoreTransformations[i].Cosine - C.CoreTransformations[n - 1 - i].Cosine.Conjugate() * d) / C.CoreTransformations[n - 1 - i].Sine;
+
+                Complex c = 0;
+
+                d = Q.CoreTransformations[i + 1].Cosine * d;
+
+                (a, c) = Q.CoreTransformations[i] * (a, c);
+                (b, d) = Q.CoreTransformations[i] * (b, d);
+
+                return (a, b, c, d);
+            }
         }
 
         public List<Complex> Solve(Polynomial polynomial)
@@ -95,20 +264,12 @@ namespace MathNet.Numerics.LinearAlgebra.Complex.Solvers
 
         public List<Complex> Solve(Complex[] coefficients)
         {
-            int n = coefficients.Length;
+            var m = coefficients.Length;
 
             // The companion matrix A is factorized into:
-            // A = Q * C * (B' + e1 * y')
+            // A = Q * C' * (B' + e1 * y')
             // where Q, C, B are unitary and stored using Givens rotations.
-
-            GivensRotation[] Q = new GivensRotation[n];
-
-            for(int i = 0; i < n-1; ++i)
-            {
-                Q[i] = new GivensRotation(0, -1);
-            }
-            Q[n-1] = new GivensRotation(1, 0);
-
+            var factorization = AmvwFactorization.Create(coefficients);
 
 
             return null;
